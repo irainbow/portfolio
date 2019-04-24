32a3ea47daed775d1b32aa90ddb8b4dd460825c4
Isha Wilson <isharainbowlight@gmail.com>
2019-04-24 09:44:56 -0700

    added provider in image component. Working now in that it isnt erroring, still dont get how to get data out of the service

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -4,24 +4,25 @@ import {GalleryService, imageData} from '../gallery.service';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
+  providers: [GalleryService]
 })
 export class GalleryComponent implements OnInit {
 
   galleryContent: imageData[] | null = null;
 
   constructor(public galleryManager: GalleryService) {
-    // this.searchImages('');
+    this.searchImages('');
     console.log('Hello');
   }
 
-  // searchImages(query: string) {
-  //   query = query.toLowerCase().trim();
+  searchImages(query: string) {
+    query = query.toLowerCase().trim();
 
-  //   this.galleryContent = this.galleryManager.images
-  //     .filter(image => image.pieceTitle.toLowerCase().includes(query))
-  //     .sort((a, b) => a.pieceTitle === b.pieceTitle ? 0 : a.pieceTitle < b.pieceTitle ? -1 : 1);
-  // }
+    this.galleryContent = this.galleryManager.images
+      .filter(image => image.pieceTitle.toLowerCase().includes(query))
+      .sort((a, b) => a.pieceTitle === b.pieceTitle ? 0 : a.pieceTitle < b.pieceTitle ? -1 : 1);
+  }
 
   ngOnInit() {
   }
